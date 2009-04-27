e61d19c58928ae291c29f1957ab9d2ba7f847709
paperworkbin@gmail.com <mr@man.com>
2009-04-26 22:46:14 -0400

    Events Updates

--- KATG.com/Classes/CustomCell.h
+++ KATG.com/Classes/CustomCell.h
@@ -18,13 +18,19 @@
 #import <UIKit/UIKit.h>
 
 @interface CustomCell : UITableViewCell {
-    UILabel *lblTitle;
+	UILabel *lblTitle;
     UILabel *lblPublish;
+	UILabel *lblPublishDate;
     UIImageView *imgSquare;
+	//UIView *backgroundView;
 }
 
 @property (nonatomic, retain) UILabel *lblTitle;
 @property (nonatomic, retain) UILabel *lblPublish;
+@property (nonatomic, retain) UILabel *lblPublishDate;
 @property (nonatomic, retain) UIImageView *imgSquare;
+//@property (nonatomic, retain) UIView *backgroundView;
+
+- (id)initWithFrame:(CGRect)frame reuseIdentifier:(NSString *)reuseIdentifier;
 
 @end
--- KATG.com/Classes/CustomCell.m
+++ KATG.com/Classes/CustomCell.m
@@ -31,26 +31,32 @@
 
 @implementation CustomCell
 
-@synthesize lblTitle, lblPublish, imgSquare;
+@synthesize lblTitle, lblPublish, lblPublishDate, imgSquare;
 
 - (id)initWithFrame:(CGRect)frame reuseIdentifier:(NSString *)reuseIdentifier {
     if (self = [super initWithFrame:frame reuseIdentifier:reuseIdentifier]) {
         // Initialization code
-        lblTitle = [[UILabel alloc] initWithFrame:CGRectZero];
+		lblTitle = [[UILabel alloc] initWithFrame:CGRectZero];
         lblTitle.textColor = [UIColor blackColor];
         lblTitle.font = [UIFont boldSystemFontOfSize:12.0];
-        lblTitle.backgroundColor = [UIColor clearColor];
 		lblTitle.numberOfLines = 4;
 		
         lblPublish = [[UILabel alloc] initWithFrame:CGRectZero];
         lblPublish.textColor = [UIColor darkGrayColor];
         lblPublish.font = [UIFont systemFontOfSize:12.0];
-        lblPublish.backgroundColor = [UIColor clearColor];
+		
+		lblPublishDate = [[UILabel alloc] initWithFrame:CGRectZero];
+        lblPublishDate.textColor = [UIColor darkGrayColor];
+        lblPublishDate.font = [UIFont systemFontOfSize:12.0];
         
-        imgSquare = [[UIImageView alloc] initWithFrame:CGRectZero];        
-        
-        [self.contentView addSubview:lblTitle];
+        imgSquare = [[UIImageView alloc] initWithFrame:CGRectZero];
+		
+		//backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
+		
+		//[self.contentView addSubview:backgroundView];
+		[self.contentView addSubview:lblTitle];
         [self.contentView addSubview:lblPublish];
+        [self.contentView addSubview:lblPublishDate];
         [self.contentView addSubview:imgSquare];
     }
     return self;
@@ -61,41 +67,49 @@
     
     CGRect baseRect = CGRectInset(self.contentView.bounds, 10, 0);
     CGRect rect = baseRect;
-    rect.origin.y += -7;
-    rect.size.width = self.contentView.bounds.size.width - 100;
 	
+	rect.origin.x = self.contentView.bounds.size.width - 190;
+    rect.origin.y = 0;
+    rect.size.width = self.contentView.bounds.size.width - 120;
     lblTitle.frame = rect;
     
-    rect.origin.y += 30;
+	rect.origin.x = self.contentView.bounds.size.width - 265;
+    rect.origin.y = 45;
+	rect.size.width = 70;
+	rect.size.height = 20;
     lblPublish.frame = rect;
+	
+	rect.origin.y -= 30;
+    lblPublishDate.frame = rect;
     
-    rect.size.width = 60;
-    rect.size.height = 40;
-    rect.origin.x = self.contentView.bounds.size.width - 80;
-    rect.origin.y += 0;
-    
+    rect.size.width = 20;
+    rect.size.height = 20;
+    rect.origin.x = self.contentView.bounds.size.width - 295;
+    rect.origin.y = 30;
     imgSquare.frame = rect;
+	
 }
-
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
     // Configure the view for the selected state
-    
     if (selected) {
         lblTitle.textColor = [UIColor whiteColor];
         lblPublish.textColor = [UIColor whiteColor];
+		lblPublishDate.textColor = [UIColor whiteColor];
     } else {
         lblTitle.textColor = [UIColor blackColor];
         lblPublish.textColor = [UIColor darkGrayColor];
+        lblPublishDate.textColor = [UIColor darkGrayColor];
     }
 }
 
 - (void)dealloc {
     [lblTitle release];
     [lblPublish release];
+	[lblPublishDate release];
     [imgSquare release];
-    
+	//[backgroundView release];
     [super dealloc];
 }
 
